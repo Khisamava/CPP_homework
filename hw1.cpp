2685d2611a9e0077c0294dc46136ef10838fbd12
Khisamava <amina.khis@icloud.com>
2024-09-10 22:07:47 +0300

    homework 7.09

---
+++ hw1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout << "Vvedite chislo povtoreniy" << endl;
+    cin >> n;
+
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Hello world" << endl;
+    }
+
+    return 0;
+}
